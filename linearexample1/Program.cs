19a4097280fafd58fd525663cc85cb4dc01a29d8
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-10-27 16:20:54 +0700

    membuat funtion input

--- linearexample1/Program.cs
+++ linearexample1/Program.cs
@@ -8,6 +8,26 @@ namespace linearexample1
 {
     internal class Program
     {
+
+        int[] arr = new int[20];
+
+        int n;
+
+        int i;
+        
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element.\n");
+            }
+        }
         static void Main(string[] args)
         {
         }
